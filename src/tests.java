d4f410d48b64195452d00973830834ddb73e57ca
ShawnN003 <Nguru.Shawn@student.greenriver.edu>
2025-04-09 15:47:16 -0700

    Implemented testing for methods

--- src/tests.java
+++ src/tests.java
@@ -64,4 +64,88 @@ public void add_TestNormalAdd(){
     assertEquals("hi", list.get(0));
 }
 
+@Test
+public void get_TestNormalGet(){
+    StringList list = new DynamicStringList();
+
+    list.add("hi");
+    
+    System.out.println();
+    assertEquals("hi", list.get(0));
+}
+
+@Test
+public void remove_TestNormalRemoveFirst(){
+    StringList list = new DynamicStringList();
+    for(int i=0; i<=5; i++){
+        list.add("hi" + i);
+    }    
+    assertEquals("hi3", list.remove(3));
+}
+
+@Test
+public void set_TestNormalFirstIndex(){
+    StringList list = new DynamicStringList();
+    for(int i=0; i<=5; i++){
+        list.add("hi" + i);
+    }    
+    list.set(0,"TESTING");
+    assertEquals("TESTING", list.get(0));
+}
+
+@Test
+public void set_TestNormalMiddleIndex(){
+    StringList list = new DynamicStringList();
+    for(int i=0; i<=5; i++){
+        list.add("hi" + i);
+    }    
+    list.set(3,"TESTING");
+    assertEquals("TESTING", list.get(3));
+}
+
+@Test
+public void set_TestNormalLastIndex(){
+    StringList list = new DynamicStringList();
+    for(int i=0; i<=5; i++){
+        list.add("hi" + i);
+    }    
+    list.set(5,"TESTING");
+    assertEquals("TESTING", list.get(5));
+}
+
+@Test
+public void size_TestNormalSize(){
+    StringList list = new DynamicStringList();
+    for(int i=0; i<=10; i++){
+        list.add("hi" + i);
+    }    
+    assertEquals(11, list.size());
+}
+
+@Test
+public void capacity_TestLessThanTen(){
+    StringList list = new DynamicStringList();
+    for(int i=0; i<=5; i++){
+        list.add("hi" + i);
+    }    
+    assertEquals(10, list.capacity());
+}
+
+@Test
+public void capacity_TestEqualsTen(){
+    StringList list = new DynamicStringList();
+    for(int i=0; i<=10; i++){
+        list.add("hi" + i);
+    }    
+    assertEquals(20, list.capacity());
+}
+
+@Test
+public void capacity_TestGreaterThanTen(){
+    StringList list = new DynamicStringList();
+    for(int i=0; i<=13; i++){
+        list.add("hi" + i);
+    }    
+    assertEquals(20, list.capacity());
+}
 }
